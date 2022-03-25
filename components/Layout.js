07075cd67dde9f0fc1ac428a74baf25522cb1a1b
chirag350 <chiraglamba350@gmail.com>
2022-03-25 17:28:36 +0530

    fix design

--- components/Layout.js
+++ components/Layout.js
@@ -505,9 +505,6 @@ export default function Swibc(prps) {
                         {children}
                         {config.ads.adsense.enabled && <GoogleAd format="fluid" />}
                     </Box>
-                    <Box display="flex" w="90%" position="fixed" bottom="0" justifySelf="center" alignSelf="center" maxH={300} marginBottom={"20px"}>
-                        {config.ads.adsense.enabled && <GoogleAd format="horizontal" />}
-                    </Box>
                 </Box>
             </Box>
         </>
