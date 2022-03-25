f6f18c5a816c5dfb45e09b847afc3556a3233a4e
chirag350 <chiraglamba350@gmail.com>
2022-03-25 17:20:39 +0530

    fix typo

--- components/Layout.js
+++ components/Layout.js
@@ -505,7 +505,7 @@ export default function Swibc(prps) {
                         {children}
                         {config.ads.adsense.enabled && <GoogleAd format="fluid" />}
                     </Box>
-                    <Box display="fixed" w="90%" position="fixed" bottom="0" justifySelf="center" alignSelf="center" h={100}>
+                    <Box display="flex" w="90%" position="fixed" bottom="0" justifySelf="center" alignSelf="center" h={100}>
                         {config.ads.adsense.enabled && <GoogleAd format="horizontal" />}
                     </Box>
                 </Box>
