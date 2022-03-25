a388a999ab5e96c42bef410acecfca7d419ca99a
chirag350 <chiraglamba350@gmail.com>
2022-03-25 17:01:03 +0530

    fix adsense design

--- components/GoogleAd.js
+++ components/GoogleAd.js
@@ -9,7 +9,7 @@ export default class Google extends React.Component {
     render() {
         return (
             <ins className="adsbygoogle"
-                style={{ display: 'flex', justifySelf: 'center', alignSelf: 'center', margin: "auto", maxWidth: this.props.isMobile ? "30%" : "80%", marginTop: "10px" }}
+                style={{ display: 'flex', justifySelf: 'center', alignSelf: 'center', margin: "auto", maxWidth: this.props.isMobile ? "30%" : "80%", marginTop: "30px" }}
                 data-ad-client={config.ads.adsense.dataaddclient}
                 data-ad-slot={config.ads.adsense.dataaddslot}
                 data-ad-format={this.props.format || "auto"}
